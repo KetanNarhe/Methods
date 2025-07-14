a9f8fb0b38692e6d25228b23d07443673ea480bd
Ketan narhe <ketannarhe02@gmail.com>
2025-07-14 05:39:47 -0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,22 @@
+package com.methods;
+
+public class Book 
+{
+   String brand;
+   double price;
+   int page;
+   String type;
+   String size;
+   
+   
+   public void displayBook()
+   {
+	   
+	   System.out.println("Brand:"+brand);
+	   System.out.println("Price:"+price);
+	   System.out.println("Pages:"+page);
+	   System.out.println("Types:"+type);
+	   System.out.println("Size:"+size);
+	   System.out.println("--------------------------------");
+   }
+}
---
+++ BookDriver.java
@@ -0,0 +1,29 @@
+package com.methods;
+
+public class BookDriver {
+
+	public static void main(String[] args) {
+		
+		
+		Book b1=new Book();
+		
+		b1.brand="ClassMate";
+		b1.price=150;
+		b1.page=200;
+		b1.type="A4";
+		b1.size="Ruled";
+		
+		b1.displayBook();
+		
+        Book b2=new Book();
+		
+		b2.brand="Kamlin";
+		b2.price=180;
+		b2.page=300;
+		b2.type="A4";
+		b2.size="Ruled";
+		
+		b2.displayBook();
+	}
+
+}
