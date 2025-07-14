40bacfec9f18730ec0f6e0f6a7a0973c456ec08a
Ketan narhe <ketannarhe02@gmail.com>
2025-07-14 04:47:31 -0700

    Add files via upload

---
+++ StaticBlock.java
@@ -0,0 +1,33 @@
+package com.methods;
+
+public class StaticBlock 
+
+ 
+{
+	  static
+	  {
+		  System.out.println("Staitc Block 1");  
+		  
+	  }
+
+	    static
+	    {
+	       System.out.println("Staitc Block 2");	
+	    }
+	    
+	    static
+	    {
+	       System.out.println("static Block 3");	
+	    }
+	    static
+	    {
+	       System.out.println("static Block 4");	
+	    }
+	    
+	public static void main(String[] args) 
+	{
+		
+		  System.out.println("Main Method ");
+	}
+
+}
